540f56b14a09ed37b360471fe996f2645ab8cf9c
Egor1515 <96645276+Egor1515@users.noreply.github.com>
2022-04-28 20:13:46 +1000

    RePlan

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -3,34 +3,39 @@ import com.codeborne.selenide.Configuration;
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import java.time.Duration;
 
-import java.util.Locale;
-import java.util.Random;
-import java.util.logging.Formatter;
+import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 
 public class CardDeliveryTest {
-    Faker faker = new Faker(new Locale("ru"));
 
 
     @Test
     void shouldSendFormV1() {
+
         Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "980x900";
+        var daysToAddForFirstMeeting = 4;
+        var daysToAddForSecondMeeting = 5;
         open("http://localhost:9999/");
         $x("//input[@placeholder = 'Город']").val(DataGenerator.Registration.generateInfo("ru").getCity());
         $x("//input[@placeholder ='Дата встречи']").doubleClick();
         $x("//input[@placeholder ='Дата встречи']").sendKeys(Keys.BACK_SPACE);
-        $x("//input[@placeholder ='Дата встречи']").val(DataGenerator.Registration.generateDate(4));
+        $x("//input[@placeholder ='Дата встречи']").val(DataGenerator.Registration.generateDate(daysToAddForFirstMeeting));
         $x("//input[@name= 'name']").val(DataGenerator.Registration.generateInfo("ru").getName());
-        $x("//input[@name= 'phone']").val(faker.numerify("+7##########"));
+        $x("//input[@name= 'phone']").val(DataGenerator.Registration.generateInfo("ru").getPhone());
         $(".checkbox__box").click();
         $(".button[role='button']").click();
-        $(".notification__title").should(Condition.visible, Duration.ofSeconds(15));
-        $(".notification__content").should(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id='success-notification']").should(Condition.visible);
+        $x("//input[@placeholder ='Дата встречи']").doubleClick();
+        $x("//input[@placeholder ='Дата встречи']").sendKeys(Keys.BACK_SPACE);
+        $x("//input[@placeholder ='Дата встречи']").val(DataGenerator.Registration.generateDate(daysToAddForSecondMeeting));
+        $(".button[role='button']").click();
+        $x("button__text").should(Condition.text("Перепланировать")).click();
+        $("button__content").should(Condition.visible);
+
 
 
 
--- src/test/java/DataGenerator.java
+++ src/test/java/DataGenerator.java
@@ -3,6 +3,7 @@ import com.github.javafaker.Faker;
 import com.github.javafaker.PhoneNumber;
 import io.netty.channel.local.LocalAddress;
 import lombok.experimental.UtilityClass;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
@@ -14,11 +15,12 @@ public class DataGenerator {
     public static class Registration {
         public static RegistrationInfo generateInfo(String locale) {
             Faker faker = new Faker(new Locale(locale));
-            return new RegistrationInfo(faker.name().fullName(),
-                    faker.phoneNumber().phoneNumber(),
+            var user = new RegistrationInfo(faker.name().fullName(),
+                    faker.numerify("+7##########"),
                     faker.address().city());
-
+            return user;
         }
+
         public static String generateDate(int days) {
             return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         }
