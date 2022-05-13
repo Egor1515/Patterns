ef59ae1a8ae88000775cdf7a1c837c5189cc51ce
Egor Bondar <egorbondar@MacBook-Air-Egor.local>
2022-05-13 13:19:57 +1000

    AddedDate

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -14,25 +14,25 @@ public class CardDeliveryTest {
         var info = DataGenerator.Registration.generateInfo("ru");
         Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "980x900";
-        var daysToAddForFirstMeeting = 4;
-        var daysToAddForSecondMeeting = 5;
+        var daysToAddForFirstMeeting = DataGenerator.Registration.generateDate(4);
+        var daysToAddForSecondMeeting = DataGenerator.Registration.generateDate(5);
         open("http://localhost:9999/");
         $x("//input[@placeholder = 'Город']").val(info.getCity());
         $x("//input[@placeholder ='Дата встречи']").doubleClick();
         $x("//input[@placeholder ='Дата встречи']").sendKeys(Keys.BACK_SPACE);
-        $x("//input[@placeholder ='Дата встречи']").val(DataGenerator.Registration.generateDate(daysToAddForFirstMeeting));
+        $x("//input[@placeholder ='Дата встречи']").val(daysToAddForFirstMeeting);
         $x("//input[@name= 'name']").val(info.getName());
         $x("//input[@name= 'phone']").val(info.getPhone());
         $(".checkbox__box").click();
         $(".button[role='button']").click();
-        $("[data-test-id='success-notification']").should(Condition.visible, Condition.text("Успешно"),Condition.text("Встреча успешно запланирована на "));
+        $("[data-test-id='success-notification']").should(Condition.visible, Condition.text("Успешно"),Condition.text("Встреча успешно запланирована на " + daysToAddForFirstMeeting));
         $x("//input[@placeholder ='Дата встречи']").doubleClick();
         $x("//input[@placeholder ='Дата встречи']").sendKeys(Keys.BACK_SPACE);
-        $x("//input[@placeholder ='Дата встречи']").val(DataGenerator.Registration.generateDate(daysToAddForSecondMeeting));
+        $x("//input[@placeholder ='Дата встречи']").val(daysToAddForSecondMeeting);
         $(".button[role='button']").click();
         $("[class ='button button_view_extra button_size_s button_theme_alfa-on-white']").click();
-        $("[data-test-id='success-notification']").should(Condition.visible,Condition.text("Встреча успешно запланирована на "));
-        $x("//*[@id=\"root\"]/div/div[1]/div[3]").should(Condition.visible);
+        $("[data-test-id='success-notification']").should(Condition.visible,Condition.text("Встреча успешно запланирована на " + daysToAddForSecondMeeting));
+
 
     }
 
